e1627774060f17dda43a1893e97b729bec06c060
Maxwell Walters <johndoe@example.com>
2023-03-29 13:18:30 -0500

    index and store paths

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -6,6 +6,7 @@ use App\Models\Chirp;
 use Illuminate\Http\Request;
 use illuminate\Http\Response;
 use illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class ChirpController extends Controller
 {
@@ -28,9 +29,16 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+      $validated = $request->validate([
+        'message' => 'required|string|max:255',
+      ]);
+
+      $request->user()->chirps()->create($validated);
+
+      return redirect(route('chirps.index'));
+    
     }
 
     /**
